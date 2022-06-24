20c609403ba64f1675947c5695d216ac653a5108
wzhlyforever <814808756@qq.com>
2022-06-24 12:15:58 +0800

    JDK源码环境搭建

---
+++ src/cn/codesheep/test/arraylist/TestArrayLIST.java
@@ -0,0 +1,20 @@
+package cn.codesheep.test.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: JavaSourceLearn
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2022-06-23 21:42
+ **/
+public class TestArrayLIST {
+
+
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("hello");
+    System.out.println("list = " + list.toString());
+  }
+}
